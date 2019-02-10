f6585cfee07c8d1c13e57fc87e2a5714cda97c7a
Tony Oreglia <tony.oreglia@safetyculture.io>
2019-02-11 07:26:54 +1100

    skeleton code

--- http-server/web-server.go
+++ http-server/web-server.go
@@ -11,11 +11,28 @@ func openSocket(a string, b string) socket {
 	return socket{}
 }
 
+func getFilename(msg string) string {
+	return msg
+}
+
+func open(filename string) string {
+	return filename
+}
+
 func main() {
 	serverSocket := openSocket(AF_INET, SOCK_STREAM)
+	address := ""
 	fmt.Print(serverSocket)
-
+	fmt.Print(address)
 	for true {
 		fmt.Println("Ready to serve...")
+		message := ""
+		filename := getFilename(message)
+		open(filename)
+		// get data
+		// send data
+		// close socket
 	}
+	// close server socket
+	// exit
 }
